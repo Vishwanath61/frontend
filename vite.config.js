7729270cf059432709a8786c42f06222916f8f06
Vishwanath P <120776288+Vishwanath61@users.noreply.github.com>
2025-03-25 21:48:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',  // Allow all network interfaces
     port: 10000 || 5173,  // Use the specified port or default to 5173
+    allowedHosts: [
+      'frontendet.onrender.com', // Add the allowed host here
+    ],
   },
   plugins: [react(), tailwindcss()],
 })
